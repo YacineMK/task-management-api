ba304359301624a6745696a14a254277164c9b96
YacineMK <merroukiyacine06@gmail.com>
2023-11-23 20:36:26 +0100

    add todo struct and array

--- main.go
+++ main.go
@@ -5,10 +5,18 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-
+type Todos struct {
+	title string 
+	todo string 
+	do bool 
+}
 
 func main() {
+	var list []Todos;
+	list = append(list , Todos{title : "test" , todo :"test",do : false})
+
 	app:= fiber.New();
+
 	fmt.Println("The server is running on port 3200");
 	app.Get("/" , func(c *fiber.Ctx) error {
 	  return c.SendString("Home page");
