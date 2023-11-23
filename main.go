bad68dae32e1dd286cc51c64ff1c07e149084f06
YacineMK <merroukiyacine06@gmail.com>
2023-11-23 18:43:56 +0100

    build the server

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
-package main
+package main 
 
 import (
-  "fmt"
-  "github.com/gofiber/fiber/v2"
+	"fmt"
+	"github.com/gofiber/fiber/v2"
 )
 
+
+
 func main() {
-   app:= fiber.New();
-    fmt.Println("the server run in port 3200")
-    app.Listen(":3200")
+	app:= fiber.New();
+	fmt.Println("The server is running on port 3200");
+	app.Get("/" , func(c *fiber.Ctx) error {
+	  return c.SendString("Home page");
+	});
+	app.Listen(":3200");
+
 }
