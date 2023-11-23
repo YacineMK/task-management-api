0dcb3fc10041dccf58c374b3967ed8537d80bc54
YacineMK <merroukiyacine06@gmail.com>
2023-11-23 20:49:41 +0100

    add check function

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 type Todos struct {
+	id int
 	title string 
 	todo string 
 	do bool 
@@ -13,11 +14,24 @@ type Todos struct {
 
 func main() {
 	var list []Todos;
-	list = append(list , Todos{title : "test" , todo :"test",do : false})
+	list = append(list , Todos{id :1 ,title : "test" , todo :"test",do : false})
 
 	app:= fiber.New();
-
 	fmt.Println("The server is running on port 3200");
+
+	app.Post("/addtodo",func(c *fiber.Ctx) error {
+		var todo Todos ;
+		var i int ;	
+		func check() string{
+			for i range list {
+				if (todo.title == list[i].title && list[i].title == false) {
+					return "you have allredy this todo ";
+				}
+			}
+		}
+		list = append();
+	})
+
 	app.Get("/" , func(c *fiber.Ctx) error {
 	  return c.SendString("Home page");
 	});
